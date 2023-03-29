ae16ad72f1a9d92a52b41a69572252eef33cefc9
samsonalxgit <samsonmanie2022@gmail.com>
2023-03-29 13:57:49 -0700

    fixing bugs

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
 """ FizzBuzz
+    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
 import sys
 
 
 def fizzbuzz(n):
     """
-    FizzBuzz function prints numbers from 1 to n separated byspace.
-
-    -For multiple of three print "Fizz" instead of the number and for multiples of five print "Buzz".
-    -For numbers which are multiple of both three and five print "FizzBuzz".
-    -Otherwise return the number itself
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+    
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
 
-    temp_result = []
-    for i in range(1, n+1):
-        if(i % 3) and (i % 5) ==0:
-            temp_result.append("FizzBuzz")
-        elif(i % 3) ==0:
-            temp_result.append("Fizz")
-        elif(i % 5) ==0:
-            temp_result.append("Buzz")
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            temp_result.append(str(i))
-    print("".join(temp_result))
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
-if __name__=='__main__':
-    if len(sys.argv)<=1:
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
@@ -37,4 +38,3 @@ if __name__=='__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-
