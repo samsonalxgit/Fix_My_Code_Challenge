b3c5d11a87b870563f9e4dede75d884b0e2c10fd
samsonalxgit <samsonmanie2022@gmail.com>
2023-03-29 13:55:21 -0700

    fixing bugs

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,7 +17,7 @@ def fizzbuzz(n):
 
     temp_result = []
     for i in range(1, n+1):
-        if(i % 15) ==0:
+        if(i % 3) and (i % 5) ==0:
             temp_result.append("FizzBuzz")
         elif(i % 3) ==0:
             temp_result.append("Fizz")
